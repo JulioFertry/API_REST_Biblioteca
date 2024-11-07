a951d66761e99b94c19655c6cc9fae559f954e2e
JulioFertry <juliofertry@gmail.com>
2024-11-07 20:30:26 +0100

    model creado

---
+++ src/main/kotlin/com/es/libreria/model/Libro.kt
@@ -0,0 +1,28 @@
+package com.es.libreria.model
+
+import jakarta.persistence.*
+import java.time.LocalDate
+
+
+@Entity
+@Table(name = "libros")
+data class Libro(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long = 0,
+
+    @Column(nullable = false)
+    var titulo: String,
+
+    var genero: String,
+
+    @Column(name = "anio_publicacion")
+    @Temporal(TemporalType.DATE) // Trata el atributo como Date
+    var anioPublicacion: LocalDate,
+
+    var precio: Double,
+
+    @ManyToOne
+    @JoinColumn(name = "autor_id", nullable = false)
+    var autor: Autor
+)
